5f4bda54e9a20a6aaa9e5ce6739a578d4e8fb283
weslley <weslleyhariellima@gmail.com>
2025-06-02 08:42:46 -0400

    Finalização do conteudo de variavel e introdução

---
+++ assets/js/Mochila_objeto.js
@@ -0,0 +1,16 @@
+let mochila = {
+    batom:'vermelho',
+    casaco:'verde',
+    documendo:'cpf',
+}
+
+
+//caso eu decida deletar algum tem da mochila antes do console para ver 
+
+
+delete mochila.batom
+
+console.log(mochila)
+//se eu chamar a mochila ela listará tudo, se eu colocar o ponto ele vai ler apenas o item
+//console.log(mochila.casaco)
+
